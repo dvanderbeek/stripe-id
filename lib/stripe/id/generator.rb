e6fe95af239232e89005bc2e33ebe196e5c08586
David Van Der Beek <earlynovrock@gmail.com>
2019-04-04 06:43:30 -0500

    configurable token length

--- lib/stripe/id/generator.rb
+++ lib/stripe/id/generator.rb
@@ -1,9 +1,10 @@
 module Stripe
   module Id
     class Generator
-      attr_reader :token
+      attr_reader :token, :token_length
 
-      def initialize(prefix)
+      def initialize(prefix, token_length: 14)
+        @token_length = token_length
         @token = "#{prefix.downcase}_#{generate_token}"
       end
 
@@ -14,11 +15,15 @@ module Stripe
       private
 
       def generate_token
-        SecureRandom.urlsafe_base64(10).gsub(/[-_]/, random_replacement.to_s)
+        SecureRandom.urlsafe_base64(token_bytes).gsub(/[-_]/, random_replacement.to_s)
       end
 
       def random_replacement
         ((0..9).to_a + ("a".."z").to_a + ("A".."Z").to_a).sample
+      end
+
+      def token_bytes
+        (token_length / (4.0 / 3.0)).truncate
       end
     end
   end
